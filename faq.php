a8aedc4adea18ea56e0cbff471117fdb8e6e5fdc
larissalemosrocha <larissalemosrocha@icloud.com>
2018-08-27 19:19:43 -0300

    olá

---
+++ faq.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> FAQ </title>
+  </head>
+  <body>
+      <h1 class="faq"> Perguntas Frequentes: </h1>
+      <li class="faq"> Como realizo minha primrira compra? </li>
+          <p class="faq"> Fazer compas na GEN-VINTAGE é muito prático e seguro. O primeiro passo é fazer o cadastro em nossa loja. Após o cadstro, entre numa das categorias, selecione os produtos desejados e clique em "Adicionar ao carrinho". </p>
+          <p class="faq">Em seguida, você escolhe a forma de pagamento e o endereço de entraga de seu pedido. Após isso, você consegue verificar informações sobre o prazo de entrega.</p>
+            <br>
+            <br>
+      <li class="faq"> Como faço o cadastro na GEN-VINTAGE? </li>
+          <p class="faq"> Para fazer seu cadastro, clique na opção "Fazer Cadastro", no topo do site. Em seguida, preencha os dados solicitados e clique em "Continuar".</p>
+            <br>
+            <br>
+      <li class="faq"> A GEN-VINTAGE possui loja física?</li>
+          <p class="faq"> Não, nossa loja é inteiramente digital, fazendo parte do comércio online, oferecemos praticidade e conforto aos clientes, que podem fazer suas compras sem sair de casa.</p>
+            <br>
+            <br>
+      <li class="faq"> Como posso entrar em contato com a GEN-VINTAGE?</li>
+          <p class="faq"> Você pode entrar em contato através do nosso CHAT, ou por e-mail: contatovintage@vintage.com, ou em nossa Central no telefone (11) 2222-2222, de segunda a sábado das 08:00 até às 23:00 horas.</p>
+            <br>
+            <br>
+      <li class="faq"> Quais são as formas de pagamento da GEN-VINTAGE?</li>
+          <p class="faq"> Os meios de pagamento da GEN-VINTAGE são: Boleto Bancário, Cartão de Crédito e Paypal.</p>
+             <p class="faq"> No Boleto Bancário o pagamento é à vista. Você deve imprimir o boleto e efetuar o pagamento em qualquer agência bancária, Casa Lotérica ou pelo internet banking. Em até 72 horas, seremos notificados e logo em seguida liberamos o seu pedido.</p>
+             <p class="faq"> No Cartão de Crédito você pode parcelar o pagamento da sua compra em até 10 vezes sem juros. Aceitamos as bandeiras  Visa, MasterCard, American Express, Dinner’s Club, Hipercard e Elo. Para garantir sua segurança, poderemos solicitar a confirmação de alguns dados por telefone. Após a confirmação do pagamento, liberamos o seu pedido.</p>
+             <p class="faq"> Para os clientes que possuem o cartão de crédito cadastrado na Paypal disponibilizamos esta opção de pagamento. Basta selecionar na tela de pagamento a opção de pagamento com Paypal, que a cobrança será automática e o pedido será realizado. Para dúvidas sobre Paypal, entre no site: www.paypal.com.br .</p>
+            <br>
+            <br>
+      <li class="faq"> Posso alterar a forma de pagamento do meu pedido?</li>
+          <p class="faq"> Sim, a forma de pagamento pode ser alterada após a realização do pedido. Basta entrar em contato no telefone: (11) 2222-2222 e solicitar a alteração.</p>
+            <br>
+            <br>
+      <li class="faq"> Comprar na GEN-VINTAGE é seguro?</li>
+          <p class="faq"> Sim, toda informação dos nossos clientes é sigilosa. Seus dados pessoais estão totalmente protegidos e só serão divulgados em caso de requisição judicial. Conheça um pouco mais da nossa política de privacidade, que pode ser acessada aqui.</p>
+            <br>
+            <br>
+      <li class="faq"> Posso cancelar e/ou trocar meu pedido?</li>
+          <p class="faq"> Você pode trocar seu produto em até 20 dias úteis após a entrega, entrando em contato no telefone: (11) 2222-2222.</p>
+          <p class="faq"> Voce pode cancelar seu produto a qualquer momento antes da liberação de entrega, entrando em contato no telefone: (11) 2222-2222.</p>
+            <br>
+            <br>
+
+
+  </body>
+</html>
