45382a5288fa0ea752ed92b2d55ceccef432532b
yabikuma <marcio.yabiku@gmail.com>
2018-09-03 18:54:39 -0300

    footer

--- footer.php
+++ footer.php
@@ -1,64 +1,61 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1, maximum-scale=1.0, user-scalable=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title></title>
+  </head>
+  <body>
 
-<head>
-	<meta charset="utf-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Sprint 1</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
-	<!--Bootstrap CSS Versão 4.0.0-->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	<!--Fontawesome 5.2.0-->
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <!-- Footer -->
+    <footer class="bg-dark text-white mt-4" >
+        <div class="container-fluid py-3">
+            <!-- <div class="row">
+                <div class="col-md-3">
+                    <h5> </h5></div>
+                <div class="col-md-3"></div>
+                <div class="col-md-3"></div>
+                <div class="col-md-3"></div>
+            </div> -->
+            <div class="row">
+                <div class="col-md-6"> <span class="small"><br><img src="img/Gen-Vintage-logo_horiz.png" alt="" height=120> </span></div>
+                <div class="col-md-3">
 
-</head>
+                </div>
 
-<body>
-
-	<footer class="container bg-dark">
-		<div class="row md-3">
-		<div class="col-12 "><hr class="bg-light"></div>
-		<div class="col-sm-4">
-			<h4 class="text-center text-white">Informações</h4>
-			<div class="list-group text-center">
-				<a href="#" class="list-group-item list-group-item-action list-group-item-primary ">Privacidade</a>
-				<a href="#" class="list-group-item list-group-item-action list-group-item-primary ">Contatos</a>
-				<a href="#" class="list-group-item list-group-item-action list-group-item-primary ">Outros</a>
-			</div>
-		</div>
-		<div class="col-sm-4">
-		<h4 class="text-center text-white" >Produtos</h4>
-			<div class="list-group text-center">
-				<a href="#" class="list-group-item list-group-item-action list-group-item-primary">Dúvidas</a>
-				<a href="#" class="list-group-item list-group-item-action list-group-item-primary">Troca</a>
-				<a href="#" class="list-group-item list-group-item-action list-group-item-primary">Outros</a>
-			</div>
-		</div>
-		<div class="col-sm-4">
-		<h4 class="text-center text-white">Social</h4>
-		<div class="list-group text-center">
-		<a class="list-group-item list-group-item-primary btn btn-outline-primary"href="">Fecebook</a>
-			<a class="list-group-item list-group-item-primary btn btn-outline-warning" href="">Instagram</a>
-			<a class="list-group-item list-group-item-primary btn btn-outline-info" href="">Twitter</a>
-
-		</div>
-
-		</div>
-		</div>
-        </div>
-        <div class="row">
-            <div class="container text-center mt-3 bg-primary">
-            <p class="mt-3 text-white">Todos os direitos reservados Gen Vintage 2018</p>
+                <div class="col-md-3 text-right small align-self-end">
+                  <ul class="list-unstyled" id="footer_icons">
+                    <li> <p> <a href="http://twitter.com" target="_blank"> <img src="img/twitter_logo.png" alt="Twitter_Logo" height=40> </a>
+                            <a href="http://instagram.com" target="_blank"> <img src="img/instagram_logo.png" alt="Instagram_Logo" height=40> </a>
+                            <a href="http://facebook.com" target="_blank"> <img src="img/facebook_logo.png" alt="Facebook_Logo" height=40> </a></p> </li>
+                  </ul>
+                  ©2018 Todos os direitos reservados.</div>
             </div>
         </div>
-	</footer><!--Fim do Rodapé-->
+    </footer>
 
-	<!--Plu-glins do Bootstrap-->
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-</body>
 
+  	<!-- ./Footer -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!--Plu-glins do Bootstrap-->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+  </body>
 </html>
